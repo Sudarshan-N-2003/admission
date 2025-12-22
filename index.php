518b40399af99aac3cdd36caa5193488398135bc
Sudarshan-N-2003 <sudarshann.cs@vvit.ac.in>
2025-12-22 15:12:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>College Admission</title>
 <link rel="stylesheet" href="assets/styles.css">
+<a href="/admin/login.php" class="admin-btn" style="float:right">
+  College Login
+</a>
 </head>
 <body>
 
