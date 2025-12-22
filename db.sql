944045f0257fb889b6bcee6aceee4fc4331923a2
Sudarshan-N-2003 <sudarshann.cs@vvit.ac.in>
2025-12-22 15:18:42 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -24,13 +24,58 @@ CREATE INDEX IF NOT EXISTS ix_submissions_student_name ON submissions ((data->>'
 
 
 
+CREATE TABLE admin_users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    role VARCHAR(30) DEFAULT 'OFFICE',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-ALTER TABLE admissions
-ALTER COLUMN document_status
-SET DEFAULT '{
-  "marks_10": "",
-  "marks_12": "",
-  "study_certificate": "",
-  "transfer_certificate": "",
-  "photo": ""
-}';
+
+
+CREATE TABLE admissions (
+    id SERIAL PRIMARY KEY,
+    application_id VARCHAR(20) UNIQUE NOT NULL,
+
+    student_name VARCHAR(100),
+    gender VARCHAR(10),
+    dob DATE,
+
+    father_name VARCHAR(100),
+    mother_name VARCHAR(100),
+
+    mobile VARCHAR(15),
+    guardian_mobile VARCHAR(15),
+    email VARCHAR(100),
+
+    state VARCHAR(50),
+    category VARCHAR(20),
+    sub_caste VARCHAR(50),
+
+    prev_combination VARCHAR(50),
+    prev_college TEXT,
+    permanent_address TEXT,
+
+    admission_through VARCHAR(20),
+
+    cet_number VARCHAR(30),
+    cet_rank VARCHAR(30),
+    seat_allotted VARCHAR(30),
+    allotted_branch VARCHAR(50),
+
+    r2_files JSONB,
+
+    document_status JSONB DEFAULT '{
+      "marks_10": "",
+      "marks_12": "",
+      "study_certificate": "",
+      "transfer_certificate": "",
+      "photo": ""
+    }',
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+
