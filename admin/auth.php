456b3f110ae1779e0318c1b0e7ff130c20f0ec84
Sudarshan-N-2003 <sudarshann.cs@vvit.ac.in>
2025-12-29 00:04:16 +0530

    Update auth.php

--- admin/auth.php
+++ admin/auth.php
@@ -1,5 +1,12 @@
 <?php
-session_start();
+// admin/auth.php
+
+// Start session ONLY if not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Check admin login
 if (empty($_SESSION['admin_id'])) {
     header("Location: login.php");
     exit;
